8ee3a74c5140058f2c1cbb0308113b04156d09dd
nandercarmo-plank <nandercarmo@Nanders-MacBook-Air.local>
2023-04-26 14:51:45 -0300

    Change deploy framework.

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 resource "vercel_project" "vercel_deploy" {
   name      = "onboarding-challenge"
-  framework = "nodejs"
+  framework = "node"
   git_repository = {
     type = "github"
     repo = "nandercarmo-plank/onboarding-challenge"
