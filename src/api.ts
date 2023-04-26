e27b35835b589e2c771775f38f820be130455ea1
nandercarmo-plank <nandercarmo@Nanders-MacBook-Air.local>
2023-04-26 15:37:45 -0300

    Add remote database connection.

--- src/api.ts
+++ src/api.ts
@@ -13,21 +13,25 @@ import { LaunchRouter } from './middleware/router/LaunchRouter';
 import { RocketRouter } from './middleware/router/RocketRouter';
 
 dataSource.initialize().then(() => {
-	console.log("DB connection done!");
+
+	console.log(`Database connection done...`);
+
+	const api = express();
+
+	api.use(logHandler);
+	api.use(corsConfig);
+	api.use(bodyParser.json());
+
+	api.use('/rocket', RocketRouter);
+	api.use('/launch', LaunchRouter);
+	api.use('/crewman', CrewmanRouter);
+	api.use('/crew', CrewRouter);
+	api.use('/', LaunchRouter);
+
+	api.use(errorHandler);
+
+	api.listen(process.env.PORT);
+
+	console.log(`Server running on port ${process.env.PORT}...`);
+
 }).catch((err) => console.error(err));
-
-const api = express();
-
-api.use(logHandler);
-api.use(corsConfig);
-api.use(bodyParser.json());
-
-api.use('/rocket', RocketRouter);
-api.use('/launch', LaunchRouter);
-api.use('/crewman', CrewmanRouter);
-api.use('/crew', CrewRouter);
-api.use('/', LaunchRouter);
-
-api.use(errorHandler);
-
-api.listen(process.env.PORT);
