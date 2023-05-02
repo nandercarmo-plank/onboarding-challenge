933bf9957c03005020656362a0841a3b44796a85
nandercarmo-plank <nandercarmo@Nanders-MacBook-Air.local>
2023-05-02 11:08:34 -0300

    Onboarding Challenge: Week 5.

--- src/middleware/log/Logger.ts
+++ src/middleware/log/Logger.ts
@@ -7,7 +7,8 @@ const RequestLogHandler = (req: Request, res: Response, next: NextFunction) => {
 };
 
 const RequestErrorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-	console.log(`[\x1b[31mERROR\x1b[0m] ${format(new Date(), 'dd/MM/yyyy HH:mm:ss')} Error on handling ${req.method} request from ${req.headers.origin} on ${req.url} - ${err.message}`);
+	console.log(`[\x1b[31mERROR\x1b[0m] ${format(new Date(), 'dd/MM/yyyy HH:mm:ss')} Error on handling ${req.method} request from ${req.headers.origin ? req.headers.origin : 'localhost:3333'} on ${req.url} - ${err.message}`);
+
 	const status = err.message.includes('404 Not Found') ? 404 : 500;
 	res.status(status).json({ status: status, message: err.message });
 	next();
