de473891218b458c6d941fe52524a8a2bee9498d
nandercarmo-plank <nandercarmo@Nanders-MacBook-Air.local>
2023-05-10 11:44:45 -0300

    Change cors policy.

--- src/middleware/config/CorsConfig.ts
+++ src/middleware/config/CorsConfig.ts
@@ -1,6 +1,6 @@
 import cors from "cors";
 
-const corsWhiteList = ['https://www.google.com'];
+const corsWhiteList = ['https://www.google.com', 'null'];
 
 const corsOption: cors.CorsOptions = {
 	origin: (origin, callback) => {
