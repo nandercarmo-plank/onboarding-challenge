aa18496ebb2d7204df62e8d6bcaf510b8f4cf09a
nandercarmo-plank <nandercarmo@Nanders-MacBook-Air.local>
2023-05-29 13:31:04 -0300

    Atualização do cors.

--- src/middleware/config/CorsConfig.ts
+++ src/middleware/config/CorsConfig.ts
@@ -1,6 +1,6 @@
 import cors from "cors";
 
-const corsWhiteList = ['https://www.google.com', 'null'];
+const corsWhiteList = ['https://www.google.com', 'null', 'http://127.0.0.1:5500', 'http://localhost:5173'];
 
 const corsOption: cors.CorsOptions = {
 	origin: (origin, callback) => {
