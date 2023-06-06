34538d9ddbb9fdd3abe7ab8fe17d3037f91a653e
nandercarmo-plank <nandercarmo@Nanders-MacBook-Air.local>
2023-06-06 15:48:29 -0300

    Atualização do cors.

--- src/middleware/config/CorsConfig.ts
+++ src/middleware/config/CorsConfig.ts
@@ -1,6 +1,6 @@
 import cors from "cors";
 
-const corsWhiteList = ['https://www.google.com', 'null', 'http://127.0.0.1:5500', 'http://localhost:5173'];
+const corsWhiteList = ['https://www.google.com', 'null', 'http://127.0.0.1:5500', 'http://localhost:5173', 'https://onboarding-challenge-frontend.vercel.app'];
 
 const corsOption: cors.CorsOptions = {
 	origin: (origin, callback) => {
